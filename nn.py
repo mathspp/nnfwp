1bb9857cac47380ef20fc6992301e76ebd18a04e
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-05-15 12:48:27 +0100

    Add ABC for activation functions.

--- nn.py
+++ nn.py
@@ -1,4 +1,6 @@
 import numpy as np
+from abc import ABC, abstractmethod
+
 
 def create_weight_matrix(nrows, ncols):
     """Create a weight matrix with normally distributed random elements."""
@@ -8,13 +10,34 @@ def create_bias_vector(length):
     """Create a bias vector with normally distributed random elements."""
     return create_weight_matrix(length, 1)
 
+
+class ActivationFunction:
+    """Class to be inherited by activation functions."""
+    @abstractmethod
+    def f(self, x):
+        """The method that implements the function."""
+        pass
+
+    @abstractmethod
+    def df(self, x):
+        """Derivative of the function with respect to its input."""
+        pass
+
 def leaky_relu(x, leaky_param = 0.1):
     """Leaky Rectified Linear Unit with default parameter."""
     return np.maximum(x, x*leaky_param)
 
+def d_leaky_relu(x, leaky_param=0.1):
+    """Derivative of the Leaky ReLU function."""
+    return np.maximum(x > 0, leaky_param)
+
 def mean_squared_error(values, expected):
     """Mean squared error between two arrays."""
     return np.mean((values - expected)**2)
+
+def d_mean_squared_error(values, expected):
+    """Derivative of the mean squared error with respect to the computed values."""
+    return 2*(values - expected)/values.size
 
 
 class Layer:
