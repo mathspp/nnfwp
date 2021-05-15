16c67dbe374de73a3510bc08b8b73d62f6f83351
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-05-15 12:29:06 +0100

    Define basic Layer class.

--- nn.py
+++ nn.py
@@ -8,3 +8,16 @@ def create_bias_vector(length):
 
 def leaky_relu(x, leaky_param = 0.1):
     return np.maximum(x, x*leaky_param)
+
+
+class Layer:
+    def __init__(self, ins, outs, act_function):
+        self.ins = ins
+        self.outs = outs
+        self.act_function = act_function
+
+        self._W = create_weight_matrix(self.outs, self.ins)
+        self._b = create_bias_vector(self.outs)
+
+    def forward_pass(self, x):
+        return self.act_function(np.dot(self._W, x) + self._b)
