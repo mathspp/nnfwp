06c52ea81ab8a5e2f460479f1d936ae03edcb0b4
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-05-15 15:11:36 +0100

    Create a network architecture.

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,5 +1,10 @@
+import sys, pathlib
+# (Ugly) workaround to enable importing from parent folder without too much hassle.
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+
 import csv
 import numpy as np
+from nn import NeuralNetwork, Layer, LeakyReLU, MSELoss
 
 def load_data(filepath, delimiter=",", dtype=float):
     """Load a numerical numpy array from a file."""
@@ -11,3 +16,10 @@ def load_data(filepath, delimiter=",", dtype=float):
     data = np.asarray(data_list, dtype=dtype)
     print("Done.")
     return data
+if __name__ == "__main__":
+    layers = [
+        Layer(784, 16, LeakyReLU()),
+        Layer(16, 16, LeakyReLU()),
+        Layer(16, 10, LeakyReLU()),
+    ]
+    net = NeuralNetwork(layers, MSELoss(), 0.001)
