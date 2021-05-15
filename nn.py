2a6e71096fc928d96b041f12bc1931c00214e294
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-05-15 12:28:18 +0100

    Implement the Leaky ReLU activation function.

--- nn.py
+++ nn.py
@@ -5,3 +5,6 @@ def create_weight_matrix(nrows, ncols):
 
 def create_bias_vector(length):
     return create_weight_matrix(length, 1)
+
+def leaky_relu(x, leaky_param = 0.1):
+    return np.maximum(x, x*leaky_param)
